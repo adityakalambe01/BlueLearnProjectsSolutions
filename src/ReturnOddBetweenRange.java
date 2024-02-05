2aadd46b840734062954a526e34cc511ca4dbe99
AdityaKalambe01 <adityakalambe20@gmail.com>
2024-02-05 10:09:42 +0530

    Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive)

--- src/ReturnOddBetweenRange.java
+++ src/ReturnOddBetweenRange.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class ReturnOddBetweenRange {
+import java.util.Scanner;
+
+class NegativeNumberException extends Exception{
+    NegativeNumberException(String message){
+        super(message);
+    }
+}
+public class ReturnOddBetweenRange {
+    static void printOdd(int low, int high){
+        int count = 0;
+        for (int i = low; i <= high; i++) {
+            if(i%2==1) count++;
+        }
+        System.out.println(count);
+    }
+
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        try{
+            int low = scanner.nextInt();
+            int high = scanner.nextInt();
+            if(low<0 || high<0){
+                throw new NegativeNumberException("Negative Number Not allowed");
+            }
+            if (low>high){
+                printOdd(high, low);
+            }else {
+                printOdd(low, high);
+            }
+        }catch (NegativeNumberException e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
