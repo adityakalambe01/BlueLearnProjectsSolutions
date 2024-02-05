30580c855afa966873ec37fd27fa9718ecca2d6c
AdityaKalambe01 <adityakalambe20@gmail.com>
2024-02-05 12:00:52 +0530

    Day 3, MUJ 21 Day Challenge

---
+++ src/CheckPalindrome.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CheckPalindrome {
+    static boolean isPalindrome(String inputString){
+        for (int i = 0; i < inputString.length()/2; i++) {
+            if (inputString.charAt(i) != inputString.charAt(inputString.length()-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String inputString = scanner.nextLine();
+
+        inputString = inputString.replaceAll(" ","").toLowerCase();
+        System.out.println(
+                isPalindrome(inputString) ? "The given String is palindrome." : "The given String is not palindrome."
+        );
+    }
+}
